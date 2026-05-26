c7e80b38a6112e58198c551016072f687c34dc8d
André Hernández <jorgeandre.hernandez@ucr.ac.cr>
2026-05-25 20:03:26 -0600

    Actualizar main, lorenz y gráficas

--- Tarea_03/lorenz_methods.cpp
+++ Tarea_03/lorenz_methods.cpp
@@ -8,7 +8,7 @@ std::vector<double> lorenz(const std::vector<double>& r, double sigma,
 	double y = r[1];
 	double z = r[2];
 	
-	std::vector L = {sigma*(y-x), x*(rho-z)-y, x*y - beta*z};
+	std::vector<double> L = {sigma*(y-x), x*(rho-z)-y, x*y - beta*z};
 	return L;
 }
 
@@ -85,31 +85,51 @@ std::vector<std::vector<double>> rk2(double h, double t0, double tf,
 
 std::vector<std::vector<double>> rk4(double h, double t0, double tf,
 					double x0, double y0, double z0){
-	std::vector<std::vector<double>> resultado;
-	return resultado;
-}
+	
+	std::vector<std::vector<double>> rk4;
+	std::vector<double> r = {x0, y0, z0};
+	rk4.push_back(r);
+	
+	std::vector<double> r_aux = {x0, y0, z0};
+	
+	int N = ((tf-t0)/h)+1;
+		
+	std::vector<double> k1;
+	std::vector<double> k2;
+	std::vector<double> k3;
+	std::vector<double> k4;
+	
+	for (int i = 0; i < N; i++){
+	
+		k1 = lorenz(r, 10, 28, 8/3);
+		
+		r_aux[0] = r[0] + (h/2)*k1[0];
+		r_aux[1] = r[1] + (h/2)*k1[1];
+		r_aux[2] = r[2] + (h/2)*k1[2];
+		
+		k2 = lorenz(r_aux, 10, 28, 8/3);
+		
+		r_aux[0] = r[0] + (h/2)*k2[0];
+		r_aux[1] = r[1] + (h/2)*k2[1];
+		r_aux[2] = r[2] + (h/2)*k2[2];
+		
+		k3 = lorenz(r_aux, 10, 28, 8/3);
+		
+		r_aux[0] = r[0] + h*k3[0];
+		r_aux[1] = r[1] + h*k3[1];
+		r_aux[2] = r[2] + h*k3[2];
 
-int main(){
+		k4 = lorenz(r_aux, 10, 28, 8/3);
 
-	double sigma = 10;
-	double rho = 28;
-	double beta = 8/3;
+		r_aux[0] = r[0] + (h/6)*(k1[0] + 2*k2[0] + 2*k3[0] + k4[0]);
+		r_aux[1] = r[1] + (h/6)*(k1[1] + 2*k2[1] + 2*k3[1] + k4[1]);
+		r_aux[2] = r[2] + (h/6)*(k1[2] + 2*k2[2] + 2*k3[2] + k4[2]);
 
-	double t0 = 0;
-	double tf = 40;
-	double x0 = 1;
-	double y0 = 2;
-	double z0 = 3;
-	
-	std::vector<std::vector<double>> metodo_euler;
-	
-	std::vector<double> r_final;
-	
-	metodo_euler = rk2(0.01, 0, 40, x0, y0, z0);
-	
-	for (int i = 0; i < metodo_euler.size(); i++){
-	std::cout << metodo_euler[i][2] << std::endl;
+		r = r_aux;
+		
+		rk4.push_back(r);
+		
 	}
 	
-	return 0;
+	return rk4;
 }
---
+++ Tarea_03/main.py
@@ -0,0 +1,61 @@
+import lorenz_methods
+import matplotlib.pyplot as plt
+import numpy as np
+
+ti = 0
+tf = 100
+
+h = 0.001
+
+xi = 1
+yi = 1
+zi = 1
+
+x_euler = []
+y_euler = []
+z_euler = []
+
+x_rk2 = []
+y_rk2 = []
+z_rk2 = []
+
+x_rk4 = []
+y_rk4 = []
+z_rk4 = []
+
+N = int(((tf-ti)/h) + 1)
+t = np.linspace(ti, tf, int(N))
+
+sol_euler = lorenz_methods.euler(h, ti, tf, xi, yi, zi)
+sol_rk2 = lorenz_methods.rk2(h, ti, tf, xi, yi, zi)
+sol_rk4 = lorenz_methods.rk4(h, ti, tf, xi, yi, zi)
+
+for euler in sol_euler:
+	x_euler.append(euler[0])
+	y_euler.append(euler[1])
+	z_euler.append(euler[2])
+	
+for rk2 in sol_rk2:
+	x_rk2.append(rk2[0])
+	y_rk2.append(rk2[1])
+	z_rk2.append(rk2[2])
+	
+for rk4 in sol_rk4:
+	x_rk4.append(rk4[0])
+	y_rk4.append(rk4[1])
+	z_rk4.append(rk4[2])
+	
+fig = plt.figure(figsize=(10,10), dpi=130)
+ax = fig.add_subplot(111, projection="3d")
+
+ax.plot(x_euler, y_euler, z_euler, linewidth=0.7)
+ax.plot(x_rk2, y_rk2, z_rk2, linewidth=0.7, color="orange")
+ax.plot(x_rk4, y_rk4, z_rk4, linewidth=0.7, color="red")
+
+
+ax.set_xlabel("Eje X")
+ax.set_ylabel("Eje Y")
+ax.set_zlabel("Eje Z")
+
+plt.show()
+
